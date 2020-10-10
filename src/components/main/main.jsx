48b6c7c8df688ca999f63279df3252030dc0a21d
Valadzkoaliaksei <Valadzkoaliaksei@tut.by>
2020-10-11 01:57:24 +0300

    Логика работает (вроде)

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,50 +1,31 @@
-import React, { useState } from 'react';
-
-import CircularProgress from '@material-ui/core/CircularProgress';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { TextField, Button } from '@material-ui/core';
+
+import { Weather } from '../weather';
 
 import './main.css';
 
-export const Main = () => {
-  const [city, setCity] = useState("");
-  const [isLoading, setLoading] = useState(false);
-  const [isLoaded, setLoaded] = useState(false);
-  const [isError, setError] = useState(false);
-  const [weather, setWeather] = useState({});
-
-  const onChangeCity = (event) => {
-    setCity(event.target.value);
-  };
-
-  const onButtonClick = async () => {
-    try {
-      const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f4a3e13f259dfdbd23f06c3973018636&units=metric&`);
-      console.log(weatherResponse);
-      let weatherNow = await weatherResponse.json();
-      console.log(weatherNow);
-      if (weatherNow.cod !== 200 )  throw new Error("Ошибка!");
-      setWeather(weatherNow);
-      setLoaded(true);
-    } catch {
-      setError(true);
-    }
-  };
-  return (
-    <div className="Form">
-      <TextField value={city} onChange={onChangeCity} />
-      <Button onClick={onButtonClick}>Показать</Button>
-      { isLoading && (<CircularProgress />)}
-      { isLoaded && !isError && (
-        <div>
-          <div>Температура: {weather.main.temp}&deg;С</div>
-          <div>Давление: {weather.main.pressure}мм.рт.ст.</div>
-          <div>Влажность: {weather.main.humidity}%</div>
-          <div>Ветер: {weather.wind.speed}м/с</div>
-          <div>Облачность: {weather.clouds.all}%</div>
-          
-        </div>
-      )}
-      { isError && (<div>Произошла ошибка</div>)}
-    </div>
-  );
-}
+export const Main = ({
+  city,
+  isLoaded,
+  isError,
+  weather,
+  onChangeCity,
+  onShow,
+  onAdd,
+  favorites
+}) => (
+  <div className="main">
+    <TextField value={city} onChange={onChangeCity} />
+    <br/>
+    <br/>
+    <Button variant="contained" color="primary" onClick={() => {onShow(city)}}>Показать</Button>
+    <br/>
+    <br/>
+    <Button variant="contained" color="secondary" onClick={onAdd}>Добавить в избранное</Button>
+    <br/>
+    {favorites.map(item => (<Link to={`/${item.link}`} onClick={() => { onShow(item.name)}}>{item.name}<br/></Link>))}
+    <Weather isLoaded={isLoaded} isError={isError} weather={weather}/>
+  </div>
+)
